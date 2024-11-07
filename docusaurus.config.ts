9e479063890921503448cd3c45b6f37bb0304abf
Tomas Baca <klaxalk@gmail.com>
2024-11-07 16:47:07 +0100

    pes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
-          lastVersion: '1.5.0',
+          lastVersion: 'current',
           versions: {
             current: {
               label: '1.5.0',
