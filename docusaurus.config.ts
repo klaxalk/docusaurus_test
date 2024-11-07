3271f5b5224335f2ccf87e2b98bcc7cb1e067f6f
Tomas Baca <klaxalk@gmail.com>
2024-11-07 15:07:44 +0100

    pes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,7 +22,7 @@ const config: Config = {
   organizationName: 'ctumrs', // Usually your GitHub org/user name.
   projectName: 'ctumrs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
