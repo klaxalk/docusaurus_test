12b880afeff77e5a89d3d3c8631ea0ac088f47c9
Tomas Baca <klaxalk@gmail.com>
2024-11-13 16:57:49 +0100

    pes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -55,11 +55,6 @@ const config: Config = {
           versions: {
             'current': {
               label: '1.5.0',
-              path: '1.5.0',
-            },
-            '1.4.9': {
-              label: '1.4.9',
-              path: '1.4.9',
             },
           },
 
